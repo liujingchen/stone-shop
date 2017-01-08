0d7f83d5451519874f5d8cc540b7ddc436e7a830
Jingchen Liu <ljc.thu@gmail.com>
2017-01-08 15:33:44 +0900

    add data store interface

---
+++ data_store.go
@@ -0,0 +1,28 @@
+package stoneshop
+
+type ItemStatus int
+
+type Item struct {
+	Id            string
+	Status        ItemStatus
+	Size          int
+	YahooUrl      string
+	BuyerName     string
+	BuyerPostCode string
+	BuyerAddress  string
+}
+
+const (
+	New ItemStatus = iota
+	HasImage
+	HasInfo
+	OnSale
+	Sold
+	Sent
+)
+
+type DataAccessor interface {
+	CreateNewItem() (string, error)
+	UpdateItem() error
+	DeleteItem() error
+}
