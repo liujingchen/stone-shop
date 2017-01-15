9fee6d068668217f287dfd20e13f469d1d5d13eb
Jingchen Liu <liu@indeed.com>
2017-01-15 14:35:34 +0900

    upload file

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const fs = require('fs');
 const bodyParser = require('body-parser');
+const multer = require('multer');
+const upload = multer({storage: multer.diskStorage({'dest': '/tmp/'})});
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 const ObjectId = mongodb.ObjectId;
+const GridStore = mongodb.GridStore;
+const GridFSBucket = mongodb.GridFSBucket;
 const MONGO_URL = 'mongodb://localhost:27017/stoneshop';
 const FAILED_TO_CONNECT = 'Failed to connect to DB.';
 const COL_ITEM = 'items';
+const COL_IMG = 'itemImg';
 const app = express();
+let db;
 app.use('/static', express.static('html/static'));
 app.use(bodyParser.urlencoded({'extended': false}));
 app.set('view engine', 'pug');
@@ -17,21 +24,15 @@ app.get('/', function(req, res) {
         step = req.query.step;
     }
 
-    MongoClient.connect(MONGO_URL, function(err, db) {
-        if(err != null) {
-            res.status(500).send(FAILED_TO_CONNECT);
-            return;
+    let col = db.collection(COL_ITEM);
+    let queryObj = buildQueryFromStep(step);
+    col.find(queryObj).toArray(function(err, docs) {
+        if (err != null) {
+            res.status(500).send('Failed to get items from DB.');
+            console.log(err);
+        } else {
+            res.render('item_list', {'items': docs});
         }
-        let col = db.collection(COL_ITEM);
-        let queryObj = buildQueryFromStep(step);
-        col.find(queryObj).toArray(function(err, docs) {
-            if (err != null) {
-                res.status(500).send('Failed to get items from DB.');
-            } else {
-                res.render('item_list', {'items': docs});
-            }
-            db.close();
-        });
     });
 });
 
@@ -41,7 +42,7 @@ function buildQueryFromStep(step) {
         case '2': return notExistQuery('size');
         case '3': return {'$and': [
             existQuery('size'),
-            // existQuery('photo'),
+            existQuery('photo'),
             notExistQuery('yahooId'),
         ]};
         case '4': return {'$and': [
@@ -74,83 +75,98 @@ app.get('/create', function(req, res) {
 });
 
 app.post('/item_create', function(req, res) {
-    MongoClient.connect(MONGO_URL, function(err, db) {
-        if(err != null) {
-            res.status(500).send(FAILED_TO_CONNECT);
-            return;
+    let col = db.collection(COL_ITEM);
+    col.insertOne(req.body, function(err, r) {
+        if (err != null || r.insertedCount != 1) {
+            res.status(500).send('Failed to insert item to DB.');
+        } else {
+            res.redirect('/item/' + r.insertedId);
         }
-        let col = db.collection(COL_ITEM);
-        col.insertOne(req.body, function(err, r) {
-            if (err != null || r.insertedCount != 1) {
-                res.status(500).send('Failed to insert item to DB.');
-            } else {
-                res.redirect('/item/' + r.insertedId);
-            }
-            db.close();
-        });
     });
 });
 
 app.get('/item/:itemId', function(req, res) {
-    MongoClient.connect(MONGO_URL, function(err, db) {
-        if(err != null) {
-            res.status(500).send(FAILED_TO_CONNECT);
-            return;
-        }
-        let col = db.collection(COL_ITEM);
-        col.find({'_id': new ObjectId(req.params.itemId)}).limit(1).toArray(
-            function(err, docs) {
-                if (err != null || docs.length != 1) {
-                    res.status(500).send('Failed to get one item from DB.');
-                } else {
-                    res.render('item', {'item': docs[0]});
-                }
-                db.close();
+    let col = db.collection(COL_ITEM);
+    col.find({'_id': new ObjectId(req.params.itemId)}).limit(1).toArray(
+        function(err, docs) {
+            if (err != null || docs.length != 1) {
+                res.status(500).send('Failed to get one item from DB.');
+            } else {
+                res.render('item', {'item': docs[0]});
             }
-        );
-    });
+        }
+    );
 });
 
 app.post('/item_update', function(req, res) {
-    MongoClient.connect(MONGO_URL, function(err, db) {
-        if(err != null) {
-            res.status(500).send(FAILED_TO_CONNECT);
-            return;
-        }
-        let col = db.collection(COL_ITEM);
-        let data = Object.assign({}, req.body);
-        delete data._id;
-        col.updateOne({'_id': new ObjectId(req.body._id)}, {'$set': data},
-            function(err, r) {
-                if (err != null) {
-                    res.status(500).send('Failed to update item to DB.');
-                } else {
-                    res.redirect('/item/' + req.body._id);
-                }
-                db.close();
+    let col = db.collection(COL_ITEM);
+    let data = Object.assign({}, req.body);
+    delete data._id;
+    col.updateOne({'_id': new ObjectId(req.body._id)}, {'$set': data},
+        function(err, r) {
+            if (err != null) {
+                res.status(500).send('Failed to update item to DB.');
+            } else {
+                res.redirect('/item/' + req.body._id);
             }
-        );
-    });
+        }
+    );
 });
 
 app.post('/item_delete', function(req, res) {
-    MongoClient.connect(MONGO_URL, function(err, db) {
-        if(err != null) {
-            res.status(500).send(FAILED_TO_CONNECT);
-            return;
+    let col = db.collection(COL_ITEM);
+    col.deleteOne({'_id': new ObjectId(req.body._id)}, function(err, r) {
+        if (err != null || r.deletedCount != 1) {
+            res.status(500).send('Failed to delete item from DB.');
+        } else {
+            res.redirect('/');
         }
-        let col = db.collection(COL_ITEM);
-        col.deleteOne({'_id': new ObjectId(req.body._id)}, function(err, r) {
-            if (err != null || r.deletedCount != 1) {
-                res.status(500).send('Failed to delete item from DB.');
-            } else {
-                res.redirect('/');
+    });
+});
+
+app.post('/img_upload', upload.single('imgFile'), function(req, res) {
+    let imgId = new ObjectId();
+    let gridStore = new GridStore(db, imgId, req.file.filename,
+            'w', {'root': COL_IMG});
+    gridStore.writeFile(req.file.path, function(err, doc) {
+        if(err != null) {
+            res.status(500).send('Failed to save img.');
+        } else {
+            let col = db.collection(COL_ITEM);
+            col.updateOne({'_id': new ObjectId(req.body._id)},
+                {'$push': {'photo': imgId}},
+                function(err, r) {
+                    if (err != null) {
+                        res.status(500).send('Failed to add img to item.');
+                    } else {
+                        res.redirect('/item/' + req.body._id);
+                    }
+                }
+            );
+        }
+        fs.unlink(req.file.path, function(err) {
+            if (err != null) {
+                console.warn('Failed to remove tmp file ' + req.file.path);
             }
-            db.close();
         });
     });
 });
 
-app.listen(8080, function() {
-    console.log('Wep app listening on port 8080!');
+app.get('/img/:imgId', function(req, res) {
+    let imgId = req.params.imgId;
+    let bucket = new GridFSBucket(db, {'bucketName': COL_IMG});
+    res.setHeader('content-type', 'image/jpg');
+    bucket.openDownloadStream(new ObjectId(imgId)).pipe(res);
+});
+
+
+MongoClient.connect(MONGO_URL, function(err, dbResult) {
+    if(err != null) {
+        res.status(500).send(FAILED_TO_CONNECT);
+        return;
+    }
+    db = dbResult;
+    app.listen(8080, function() {
+        console.log('Wep app listening on port 8080!');
+    });
 });
